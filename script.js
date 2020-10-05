a88b594bebfb7fab9a0248bcda0ef97e0c18ac61
Rossana96 <36312969+Rossana96@users.noreply.github.com>
2020-10-04 23:27:58 -0500

    Update script.js

--- script.js
+++ script.js
@@ -17,4 +17,6 @@ else {
 
 var today = new Date();
 var date = today.getDate()+'-'+(today.getMonth()+1)+'-'+today.getFullYear();
+var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+var dateTime = date+' '+time;
 document.getElementById('time').innerHTML=today;
